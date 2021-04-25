18bf75539576ba9d44923233cdaae314d69f5031
Simone <startae14@gmail.com>
2021-04-25 19:03:17 +0200

    fix the errors with the different attributes in JSX

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-  return 'Click on Me!';
-}
-
 // Create a React Component
 const App = () => {
+  const buttonText = {
+    text: 'Click me'
+  };
+  const labelText = 'Enter name:';
   return (
     <div>
-      <label className="label" for="name">
-        Enter name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{backgroundColor: 'blue', color: 'white'}}>
-        {getButtonText()}
+        {buttonText.text}
       </button>
     </div>
   )
